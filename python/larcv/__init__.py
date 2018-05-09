afb912fede0bb7a384b3d7130e9e9fa3b0eb070b
Taritree <taritree.wongjirad@gmail.com>
2018-05-09 17:22:07 -0400

    save

--- python/larcv/__init__.py
+++ python/larcv/__init__.py
@@ -13,9 +13,11 @@ for l in [x for x in os.listdir(larcv_dir) if x.endswith('.so')]:
 from ROOT import larcv
 larcv.Vertex
 larcv.CSVData
+larcv.load_rootutil
 ##k=larcv.logger # this line to load C++ functions
 if 'LARCV_NUMPY' in os.environ and os.environ['LARCV_NUMPY'] == '1':
     larcv.load_pyutil
 if 'LARCV_OPENCV' in os.environ and os.environ['LARCV_OPENCV'] == '1':
     larcv.load_cvutil
 #larcv.LoadImageMod
+
